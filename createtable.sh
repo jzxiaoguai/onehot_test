c4efa1179e0cd487e98f8fe15801e57fecbd56ae
jzxiaoguai <1165528099@qq.com>
2017-10-31 16:17:59 +0800

    Update createtable.sh

--- createtable.sh
+++ createtable.sh
@@ -2,9 +2,8 @@
 #and the create table words are here
 
 sql="drop table databases_name.table_name;
-create table databases_name.table_name(label string,v1 string,v2 string,v3 string,v4 string,v5 string,v6 string,v7 string,
-v8 string,v9 string,v10 string,v11 string,v12 string,v13 string,v14 string,v15 string,v16 string,v17 string,v18 string,
-v19 string,v20 string,v21 string,v22 string,v23 string,v24 string,v25 string,v26 string) stored as parquet;
+create table databases_name.table_name(label int,v1 float,v2 float,v3 float,v4 float,v5 float,v6 float,v7 float,v8 float,v9 float,v10 float,v11 float,v12 float,v13 float,v14 float,v15 float,v16 float,v17 float,
+v18 float,v19 float,v20 float,v21 float,v22 float,v23 float,v24 float,v25 float,v26 float) stored as parquet;
 "
 sql2="insert into table sec_dw.drunk_merge_x_0
 select *, 1 as tag from table1 union all select *, 2 as tag from table2
